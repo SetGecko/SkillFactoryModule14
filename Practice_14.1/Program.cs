07933b2290bfdb5afed0dec9e0fb11b6ab5d69da
Алексей <setgecko@gmail.com>
2023-05-03 10:05:46 +0300

    Практика 14.1

--- Practice_14.1/Program.cs
+++ Practice_14.1/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Practice_14._1
+﻿using System.Linq;
+namespace Practice_14._1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            var selectedPeople = from p in people // промежуточная переменная p 
+                                 where p.StartsWith("А") // фильтрация по условию
+                                 orderby p // сортировка по возрастанию (дефолтная)
+                                 select p; // выбираем объект и сохраняем в выборку
+
+            foreach (string s in selectedPeople)
+                Console.WriteLine(s);
         }
     }
 }
