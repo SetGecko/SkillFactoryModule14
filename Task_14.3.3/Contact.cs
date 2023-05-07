1f770002d9e1f8411909b4912658513bee366c6b
Алексей <setgecko@gmail.com>
2023-05-07 06:10:38 +0300

    Выполнил задание 14.3.3

---
+++ Task_14.3.3/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14._3._3
+{
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, string email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string Name { get; }
+        public string LastName { get; }
+        public long PhoneNumber { get; }
+        public string Email { get; }
+    }
+}
